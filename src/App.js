12e4d0fc5cf2e34c8bad5a89ef04c86baed9e9de
Baburajan03 <baburajan.r03@gmail.com>
2023-09-02 20:46:22 +0530

    day3

--- src/App.js
+++ src/App.js
@@ -4,27 +4,27 @@
 //     <div><p>Hello world</p></div>
 //   );
 // }
-
 // export default App;
 
-import React from 'react'
+// import React from 'react'
 
-function Hello(){
-  return(
-    <div>
-      <p>Hello world!</p>
-    </div>
-    
-  )
-}
-function App() {
-  return (
-    <div>
-     <p>Greetings</p>
-     <Hello />
-      </div>
-    
-  )
-}
+// function Hello(){
+//   return(
+//     <div>
+//       <p>Hello world!</p>
+//     </div>   
+//   )
+// }
+// function App() {
+//   return (
+//     <div>
+//      <p>Greetings</p>
+//      <Hello />
+//       </div>  
+//   )
+// }
+// export default App
 
-export default App
+// day 3
+
+
