a9c011c110a273a9d8b14971b462df4e04e0e982
Baburajan03 <baburajan.r03@gmail.com>
2023-09-10 18:53:28 +0530

    day3

--- src/App.js
+++ src/App.js
@@ -27,4 +27,172 @@
 
 // day 3
 
+// import React from 'react'
 
+// function App(props) {
+//   const {notes} = props
+//   console.log(notes)
+//   return (
+//     <div>
+//       <h1>Notes</h1>
+//       <ul>
+//         <li>{notes[0].contents}</li>
+//         <li>{notes[1].contents}</li>
+//         <li>{notes[2].contents}</li>
+//         <li>{notes[3].contents}</li>
+//         <li>{notes[4].contents}</li>
+//       </ul>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// import React from 'react'
+
+// function App(props) {
+//   const {notes} = props
+ 
+//   return (
+//     <div>
+//       <h1>Notes</h1>
+//       <ul>
+//         {notes.map(note => <li key ={note.id}>{note.contents}</li>)}
+//       </ul>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// import React from 'react'
+
+// function Note({note}){
+
+//   return <li >{note.contents}</li>
+// }
+
+// function App({notes}) {
+//   // const {notes} = props
+ 
+//   return (
+//     <div>
+//       <h1>Notes</h1>
+//       <ul>
+//         {notes.map(note => <Note key ={note.id} note = {note}/>)}
+//       </ul>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// import React from 'react'
+
+// function Note({note}){
+//   // 1. using if
+
+//   // if(note.important){
+//   //   return <li >{note.contents + '★'}</li>
+//   // }
+//   // else{
+//   //   return <li >{note.contents}</li>
+//   // }
+
+//   // 2.using ternary operator
+//   // return <li >{note.important? note.contents + '★' : note.contents}</li>
+
+//   // 3.using logical operator
+//   return(
+//   <li>
+//     {note.contents} {note.important && '★'}
+//   </li>
+//   )
+// }
+
+// function App({notes}) {
+//   // const {notes} = props
+ 
+//   return (
+//     <div>
+//       <h1>Notes</h1>
+//       <ul>
+//         {notes.map(note => <Note key ={note.id} note = {note}/>)}
+//       </ul>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// Topic : event handling
+// import React from 'react'
+
+// function Note({note}){
+  
+//   return(
+//   <li>
+//     {note.contents} {note.important && '★'}
+//   </li>
+//   )
+// }
+
+// function App({notes}) {
+//   // const {notes} = props
+
+//   const addNote = (event) => {
+//     event.preventDefault();
+//     console.log(event.target)
+//   }
+ 
+//   return (
+//     <div>
+//       <h1>Notes</h1>
+//       <ul>
+//         {notes.map(note => <Note key ={note.id} note = {note}/>)}
+//       </ul>
+//       <form onSubmit={addNote}>
+//         <input />
+//         <button type='submit'>Save</button>
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import React from 'react'
+
+function Note({note}){
+  
+  return(
+  <li>
+    {note.contents} {note.important && '★'}
+  </li>
+  )
+}
+
+function App({notes}) {
+  // const {notes} = props
+
+  const addNote = (event) => {
+    event.preventDefault();
+    console.log(event.target)
+  }
+ 
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => <Note key ={note.id} note = {note}/>)}
+      </ul>
+      <form onSubmit={addNote}>
+        <input />
+        <button type='submit'>Save</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -3,11 +3,31 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const notes = [
+   { id : 1,
+    contents : 'working with lists',
+    important : true
+   },
+   {
+    id : 2,
+    contents : 'usage of keys',
+    important : false
+   },
+   {
+    id : 3,
+    contents : 'conditional rendering',
+    important : true
+   },
+   {
+    id : 4,
+    contents : 'handling events',
+    important : true
+   },
+   {
+    id : 5,
+    contents : 'props vs state',
+    important : false
+   }
+]
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<App notes ={ notes}/>)
