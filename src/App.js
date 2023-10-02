3017af2ab07112d9d20920bd53de0abaea2db8dc
Baburajan03 <baburajan.r03@gmail.com>
2023-10-02 17:48:32 +0530

    day5

--- src/App.js
+++ src/App.js
@@ -289,18 +289,223 @@
 
 // TOPIC : REUSABLE COMPONENT
 
-import React, { Component } from 'react'
-import Hello from './Components/Hello'
+// import React, { Component } from 'react'
+// import Hello from './Components/Hello'
 
 
-export class App extends Component {
-  render() {
-    return (
-      <div>
-        <Hello/>
-      </div>
-    )
+// export class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Hello/>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
+
+// Day 5
+// TOPIC : USESTATE HOOK
+
+// import React, {useState} from 'react'
+
+// function App() {
+
+//   const [counter,setCounter] = useState(0)
+
+//   setTimeout( () => setCounter (counter + 1),1000)
+
+//   return (
+//     <div>{counter}</div>
+//   )
+// }
+
+// export default App
+
+
+//  import React, {useState} from 'react'
+
+// function App() {
+
+//   const [counter,setCounter] = useState(0)
+
+//   const handleEvent = () => {
+//     setCounter(counter + 1)
+//   }
+  
+//   const handleEventMinus = () => {
+//     if(counter >0){
+//     setCounter(counter - 1)
+//   }}
+
+//   const handleEventzero = () => {
+//     setCounter(0)
+//   }
+
+
+//   return (
+//     <div>
+//       <div>{counter}</div>
+//     <button onClick = {handleEvent} >Plus</button>
+//     <button onClick = {handleEventMinus}>Minus</button>
+//     <button onClick = {handleEventzero} >Reset</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// TOPIC : PASSING EVENTS FROM CHILD TO PARENT COMPONENTS
+
+// import React, {useState} from 'react'
+
+// const Display = (props) => {
+//   return(
+//   <div>{props.counter}</div>
+// )}
+
+// function Button(props){
+//   return(
+//     <button onClick ={props.handleClick} >{props.text}</button>
+//   )
+
+// }
+
+
+// function App() {
+
+//   const [counter,setCounter] = useState(0)
+
+  
+
+//   const handleEvent = () => {
+//     setCounter(counter + 1)
+//   }
+  
+//   const handleEventMinus = () => {
+//     if(counter >0){
+//     setCounter(counter - 1)
+//   }}
+
+//   const handleEventzero = () => {
+//     setCounter(0)
+//   }
+
+
+//   return (
+//     <div>
+//       <Display counter = {counter}/>
+//     <Button text = 'plus' handleClick = {handleEvent}/>
+//     <Button text = 'Minus' handleClick = {handleEventMinus}/>
+//     <Button text = 'Zero' handleClick = {handleEventzero}/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// TOPIC : UseEffect
+
+// import React,{useState , useEffect} from 'react'
+
+// function App() {
+//   const [data,setData] = useState(null)
+
+//   useEffect(() => {
+//     fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then((response => response.json()))
+//     .then((data => setData(data)))
+//   },[])
+//   return (
+//     <div>
+//       <h1>API DATA</h1>
+//       {
+//         data ? (
+//           <ul>
+//       {
+//       data.map((items) => <li key = {items.id}>{items.title}</li>  )
+//       }
+//        </ul>
+//         ) : (
+//           <p>Loading data...</p>
+//         )
+//       }
+    
+   
+//     </div>
+//   )
+// }
+
+// export default App
+
+// TOPIC : PROPS DRILLING
+// PASSING PARENT TO NESTED CHILD COMPONENTS, PARENT TO CHILD TO CHILD
+
+// import React from 'react'
+
+// function GrandChildComponent({parentData}){
+//   console.log(`from grand child component: ${parentData}`);
+//   return(
+//     <div>
+//       <h3>Grand Child Component</h3>
+//     </div>
+//   )
+// }
+
+// function ChildComponent({parentData}){
+//   console.log(`from child component: ${parentData}`)
+//   return(
+//     <div>
+//       <h2>Child Component</h2>
+//       <GrandChildComponent parentData = {parentData}/>
+//     </div>
+//   )
+// }
+
+// function App() {
+//   const parentData = 'Hello from Parent'
+//   return (
+//     <div>
+//       <h1>Parent Component</h1>
+//       <ChildComponent parentData = {parentData} />
+//       </div>
+//   )
+// }
+
+// export default App
+
+// TOPIC : PASSING DATA FROM CHILD COMPONENT TO PARENT COMPONENT
+
+import React,{useState} from 'react'
+
+function ChildComponent({onMessage}) {
+  const message = 'Hello from Child'
+
+  const handleClick =() =>{
+    onMessage(message)
   }
+  return(
+    <div>
+      <h2>Child Component</h2>
+      <button onClick={handleClick}>Send Message to the parent</button>
+    </div>
+  )
+}
+
+function App() {
+  const [message, setMessage] = useState('')
+
+  const handleMessage = (data) => {
+    setMessage(data)
+  }
+  return (
+    <div>
+      <h2>Parent Component</h2>
+      <p>Message from child component : {message}</p>
+      <ChildComponent onMessage={handleMessage}/>
+    </div>
+  )
 }
 
 export default App
